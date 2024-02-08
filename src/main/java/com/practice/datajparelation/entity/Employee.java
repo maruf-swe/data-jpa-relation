cf4b46dee1aaa39cdc5e17857bd3f5031ab92c17
Maruf <marufur1847@gmail.com>
2024-02-08 23:58:21 +0600

    entity table

--- src/main/java/com/practice/datajparelation/entity/Employee.java
+++ src/main/java/com/practice/datajparelation/entity/Employee.java
@@ -1,17 +1,34 @@
 package com.practice.datajparelation.entity;
 
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Employee {
 
     @Id
+    @GeneratedValue
     private Integer empId;
     private String Identifier;
     private String firstName;
     private String lastName;
     private String email;
     private LocalDate birthDate;
+
+    
+    @Enumerated(EnumType.STRING)
+    private EmployeeRole employeeRole;
+
+    private Address address;
 
 }
