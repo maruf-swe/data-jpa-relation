a643487c6b3520cde562297cacd26d55d57abb4b
Maruf <marufur1847@gmail.com>
2024-02-08 23:52:57 +0600

    add employee table

--- src/main/java/com/practice/datajparelation/entity/Address.java
+++ src/main/java/com/practice/datajparelation/entity/Address.java
@@ -1,7 +1,16 @@
 package com.practice.datajparelation.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Address {
 
+    @Id
     private Integer id;
     private String streetName;
+
+    private  String houseNumber;
+    private String zipCode;
 }
--- src/main/java/com/practice/datajparelation/entity/Employee.java
+++ src/main/java/com/practice/datajparelation/entity/Employee.java
@@ -1,4 +1,17 @@
 package com.practice.datajparelation.entity;
 
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
 public class Employee {
+
+    @Id
+    private Integer empId;
+    private String Identifier;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate birthDate;
+
 }
