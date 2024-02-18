23eabf13318327b862ab070f645019661fd5251b
Maruf <marufur1847@gmail.com>
2024-02-18 23:49:26 +0600

    minor change

--- src/main/java/com/practice/datajparelation/entity/Address.java
+++ src/main/java/com/practice/datajparelation/entity/Address.java
@@ -5,10 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @AllArgsConstructor
